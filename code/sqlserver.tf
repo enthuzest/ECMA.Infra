aae25bd3602e94de812d0c053d178c0067c05369
faraz ahmad siddiqui <faraz841@gmail.com>
2022-04-12 23:07:38 +0530

    sql bug fix

--- code/sqlserver.tf
+++ code/sqlserver.tf
@@ -19,8 +19,8 @@ resource "azurerm_mssql_firewall_rule" "firewall_rule" {
 resource "azurerm_mssql_firewall_rule" "sql_admin_firewall_rule" {
   name                = "faraz ip address"
   server_id           = azurerm_mssql_server.primary_server.id
-  start_ip_address    = "122.163.251.223"
-  end_ip_address      = "122.163.251.223"
+  start_ip_address    = "106.214.229.221"
+  end_ip_address      = "106.214.229.221"
 }
 
 resource "azurerm_mssql_database" "primary_db" {
@@ -30,8 +30,8 @@ resource "azurerm_mssql_database" "primary_db" {
   tags = var.tags
 }
 
-resource "azurerm_mssql_database_extended_auditing_policy" "example" {
-  database_id                             = azurerm_mssql_server.primary_server.id
+resource "azurerm_mssql_database_extended_auditing_policy" "extended_policy" {
+  database_id                             = azurerm_mssql_database.primary_db.id
   storage_endpoint                        = azurerm_storage_account.sa.primary_blob_endpoint
   storage_account_access_key              = azurerm_storage_account.sa.primary_access_key
   storage_account_access_key_is_secondary = true
