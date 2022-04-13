d4bde00ad8fc8bb27cd6cc70e37e82d10436c41f
faraz ahmad siddiqui <faraz841@gmail.com>
2022-04-13 19:07:25 +0530

    azurerm version bug fix

--- code/providers.tf
+++ code/providers.tf
@@ -1,11 +1,16 @@
 terraform {
   required_version = ">=0.14.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.1.0"
+    }
+  }
   backend "azurerm" {}
 }
 
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
-  version         = "=3.1.0"
   features {}
 }
